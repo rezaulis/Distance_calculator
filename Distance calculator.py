ae1a3a2e3f7d052f86aff63a0e693bab9208c30f
Rezaul Islam <121986084+rezaulis@users.noreply.github.com>
2024-01-11 18:05:48 -0500

    Add files via upload

--- Distance calculator.py
+++ Distance calculator.py
@@ -1,16 +1,16 @@
-v = int(input("Enter the speed of the vehicle in mph: "))
-h = int(input("Enter the number of hours traveled: "))      
+speed = int(input("Enter the speed of the vehicle in mph: "))
+hour = int(input("Enter the number of hours traveled: "))      
 
 print("Hour             Distance Traveled")
 print("----------------------------------")
 
 
-def table(v, h):
+def table(speed, hour):
     sum = 0
-    for i in range(1, h+1):       
-        x = float((i) * v)
+    for i in range(1, hour+1):       
+        x = float((i) * speed)
         sum = sum + x
         print(i,"                  ",x)
     print("The accumalated distance traveled is: ", sum)
 
-table(v, h)
+table(speed, hour)
